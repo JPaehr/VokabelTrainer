26b054b555b926f47dbf6af986badca9b46848b6
JPaehr <j.paehr@gmx.de>
2013-10-14 22:36:18 +0200

    Layout von sprache, buch und lektion ändern und Wörterbuch resizeable gemacht

--- windows/WindowSpracheAendern.py
+++ windows/WindowSpracheAendern.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'c:\Users\Johannes\Documents\Python\VokabelTrainer\UIs\WindowSpracheAendern.ui'
 #
-# Created: Thu Oct 03 11:02:40 2013
+# Created: Mon Oct 14 22:16:59 2013
 #      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,31 +26,37 @@ except AttributeError:
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(400, 300)
+        Form.resize(382, 208)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.groupBox = QtGui.QGroupBox(Form)
-        self.groupBox.setGeometry(QtCore.QRect(20, 20, 361, 61))
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.groupBox)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
         self.cBSpracheAuswaehlen = QtGui.QComboBox(self.groupBox)
-        self.cBSpracheAuswaehlen.setGeometry(QtCore.QRect(10, 20, 341, 22))
         self.cBSpracheAuswaehlen.setObjectName(_fromUtf8("cBSpracheAuswaehlen"))
+        self.verticalLayout_3.addWidget(self.cBSpracheAuswaehlen)
+        self.verticalLayout.addWidget(self.groupBox)
         self.groupBox_2 = QtGui.QGroupBox(Form)
-        self.groupBox_2.setGeometry(QtCore.QRect(20, 90, 361, 61))
         self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
+        self.verticalLayout_4 = QtGui.QVBoxLayout(self.groupBox_2)
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
         self.tfNeuerName = QtGui.QLineEdit(self.groupBox_2)
-        self.tfNeuerName.setGeometry(QtCore.QRect(10, 20, 341, 20))
         self.tfNeuerName.setObjectName(_fromUtf8("tfNeuerName"))
-        self.widget = QtGui.QWidget(Form)
-        self.widget.setGeometry(QtCore.QRect(20, 250, 361, 25))
-        self.widget.setObjectName(_fromUtf8("widget"))
-        self.horizontalLayout = QtGui.QHBoxLayout(self.widget)
-        self.horizontalLayout.setMargin(0)
+        self.verticalLayout_4.addWidget(self.tfNeuerName)
+        self.verticalLayout.addWidget(self.groupBox_2)
+        self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.btnAbbrechen = QtGui.QPushButton(self.widget)
+        self.btnAbbrechen = QtGui.QPushButton(Form)
         self.btnAbbrechen.setObjectName(_fromUtf8("btnAbbrechen"))
         self.horizontalLayout.addWidget(self.btnAbbrechen)
-        self.btnSpeichernUndSchliessen = QtGui.QPushButton(self.widget)
+        self.btnSpeichernUndSchliessen = QtGui.QPushButton(Form)
         self.btnSpeichernUndSchliessen.setObjectName(_fromUtf8("btnSpeichernUndSchliessen"))
         self.horizontalLayout.addWidget(self.btnSpeichernUndSchliessen)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
