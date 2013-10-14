e1a39c2d3b8689df9f1f3341ac14a6e8f83d0099
JPaehr <j.paehr@gmx.de>
2013-10-14 23:10:55 +0200

    Layout vom Buch resizeable gemacht

--- windows/WindowBuchAnlegen.py
+++ windows/WindowBuchAnlegen.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\Johannes\Documents\Python\VokabelTrainer\UIs\WindowBuchAnlegen.ui'
+# Form implementation generated from reading ui file 'C:\Users\Johannes\Documents\Python\VokabelTrainer\UIs\WindowBuchAnlegen.ui'
 #
-# Created: Thu Sep 26 20:24:05 2013
+# Created: Mon Oct 14 23:06:29 2013
 #      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,40 +26,49 @@ except AttributeError:
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(400, 300)
+        Form.resize(402, 215)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.label = QtGui.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(20, 20, 141, 16))
         self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
         self.groupBox = QtGui.QGroupBox(Form)
-        self.groupBox.setGeometry(QtCore.QRect(10, 70, 371, 71))
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.verticalLayout_3 = QtGui.QVBoxLayout(self.groupBox)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
         self.cBSprache = QtGui.QComboBox(self.groupBox)
-        self.cBSprache.setGeometry(QtCore.QRect(10, 30, 351, 22))
         self.cBSprache.setObjectName(_fromUtf8("cBSprache"))
+        self.verticalLayout_3.addWidget(self.cBSprache)
+        self.verticalLayout.addWidget(self.groupBox)
         self.groupBox_2 = QtGui.QGroupBox(Form)
-        self.groupBox_2.setGeometry(QtCore.QRect(10, 150, 371, 61))
         self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
+        self.verticalLayout_4 = QtGui.QVBoxLayout(self.groupBox_2)
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
         self.tfBuchtitel = QtGui.QLineEdit(self.groupBox_2)
-        self.tfBuchtitel.setGeometry(QtCore.QRect(10, 30, 351, 20))
         self.tfBuchtitel.setObjectName(_fromUtf8("tfBuchtitel"))
-        self.layoutWidget = QtGui.QWidget(Form)
-        self.layoutWidget.setGeometry(QtCore.QRect(10, 250, 371, 25))
-        self.layoutWidget.setObjectName(_fromUtf8("layoutWidget"))
-        self.horizontalLayout = QtGui.QHBoxLayout(self.layoutWidget)
-        self.horizontalLayout.setMargin(0)
+        self.verticalLayout_4.addWidget(self.tfBuchtitel)
+        self.verticalLayout.addWidget(self.groupBox_2)
+        self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.btnAbbrechen = QtGui.QPushButton(self.layoutWidget)
+        self.btnAbbrechen = QtGui.QPushButton(Form)
         self.btnAbbrechen.setObjectName(_fromUtf8("btnAbbrechen"))
         self.horizontalLayout.addWidget(self.btnAbbrechen)
-        self.btnAnlegen = QtGui.QPushButton(self.layoutWidget)
+        self.btnAnlegen = QtGui.QPushButton(Form)
         self.btnAnlegen.setObjectName(_fromUtf8("btnAnlegen"))
         self.horizontalLayout.addWidget(self.btnAnlegen)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout.setStretch(1, 1)
+        self.verticalLayout.setStretch(2, 1)
+        self.verticalLayout.setStretch(3, 1)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(_translate("Form", "Form", None))
+        Form.setWindowTitle(_translate("Form", "Neues Buch anlegen", None))
         self.label.setText(_translate("Form", "Neues Buch anlegen", None))
         self.groupBox.setTitle(_translate("Form", "Sprache", None))
         self.groupBox_2.setTitle(_translate("Form", "Buchtitel", None))
