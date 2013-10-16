25c3df25a5475e1cc6603a95833185155b6f004c
JPaehr <j.paehr@gmx.de>
2013-10-16 16:46:44 +0200

    Window Abfrage Einstellungen sind nun resizeable

--- oberfaechen/Abfrage.py
+++ oberfaechen/Abfrage.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Created on 08.10.2013
 
 @author: Johannes
-'''
+"""
 from PyQt4 import QtGui, QtCore
 from windows.WindowAbfrage import Ui_Form as WindowAbfrage
 import models.base as Datenbank
 import models.Leve as leve
-import thread
 from time import sleep
 from random import shuffle as zufall
 from oberfaechen.MeintenSie import MeintenSie
+
 
 class ZeitThread(QtCore.QThread):
     def __init__(self, zeit):
@@ -19,84 +19,97 @@ class ZeitThread(QtCore.QThread):
         self.zeit = zeit
         
     def run(self):
+        """
+        überschrieben runMethode aus QThread
+        """
         sleep(self.zeit)
         print "Thread ist fertig"
         return  
  
 
 class Abfrage(WindowAbfrage, QtGui.QWidget):
-    def __init__(self, parent, lektionenIds, AbfrageHaeufigkeit, verzoegerung, meintenSie, RichtigeAnzeigen, Richtung, distanz):
+    """
+    Fenster für die Abfrage
+    """
+    def __init__(self, parent, lektionen_ids, abfrage_haeufigkeit, verzoegerung, meintenSie, RichtigeAnzeigen, Richtung, distanz):
         super(Abfrage, self).__init__(parent)
         QtGui.QWidget.__init__(self, parent=None)
         self.setupUi(self)
         
-        self.fontDick = QtGui.QFont()
-        self.fontDick.setBold(True)
-        self.fontNormal = QtGui.QFont()
-        
-        
-        self.Treffer = leve.Treffer(distanz)
+        self.font_dick = QtGui.QFont()
+        self.font_dick.setBold(True)
+        self.font_normal = QtGui.QFont()
+
+        self.treffer = leve.Treffer(distanz)
            
         zeit = float(verzoegerung)*10**(-3)  
         self.thread = ZeitThread(zeit)
         
         self.parent = parent
         
-        self.connect(self.thread, QtCore.SIGNAL("finished()"), self.weitereVokabel)
+        self.connect(self.thread, QtCore.SIGNAL("finished()"), self.weitere_vokabel)
         self.connect(self.btnWeiter, QtCore.SIGNAL("clicked()"), self.weitereVokabelKlick)
         self.btnWeiter.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Return))
         
-        self.meintenSie = meintenSie
-        self.Lektionsliste = []
+        self.meinten_sie = meintenSie
+        self.lektionsliste = []
         self.verzoegerung = verzoegerung
-        self.idAktuell = 0
-        self.RichtigeAnzeigen = RichtigeAnzeigen
-        self.Richtung = Richtung
+        self.id_aktuell = 0
+        self.vokabel_fremd = ""
+        self.richtige_anzeigen = RichtigeAnzeigen
+        self.richtung = Richtung
         self.labPunkte.setText('0')
+        self.lektion = ""
+        self.buch = ""
+        self.vokabel_deutsch = ""
+        self.datenbank = Datenbank.base("VokabelDatenbank.sqlite")
         
-        self.Datenbank = Datenbank.base("VokabelDatenbank.sqlite") 
+        self.vokabel_ids = self.lektionsid_to_vokid(lektionen_ids, int(abfrage_haeufigkeit))
         
-        self.vokabelIds = self.LektionsIdToVokId(lektionenIds, int(AbfrageHaeufigkeit))
+        self.weitere_vokabel()
         
-        self.weitereVokabel()
-        
-    def weitereVokabel(self):
-        if self.idAktuell < len(self.vokabelIds):
-            
-        
-            daten = self.Datenbank.getDataAsList("select lektionen.name, vokabeln.deutsch, vokabeln.fremd, buecher.name from vokabeln\
+    def weitere_vokabel(self):
+        """
+        weitere Vokabel ziehen
+        """
+        if self.id_aktuell < len(self.vokabel_ids):
+
+            daten = self.datenbank.getDataAsList("select lektionen.name, vokabeln.deutsch, vokabeln.fremd, buecher.name from vokabeln\
             join lektionen on (lektionen.id=vokabeln.idlektion) \
             join buecher on (buecher.id=lektionen.idBuch) \
-            where vokabeln.id like "+str(self.vokabelIds[self.idAktuell]))
+            where vokabeln.id like "+str(self.vokabel_ids[self.id_aktuell]))
             self.lektion = daten[0][0]
-            self.vokabelDeutsch = unicode(daten[0][1])
-            self.vokabelFremd = daten[0][2]
-            self.Buch = daten[0][3]
+            self.vokabel_deutsch = unicode(daten[0][1])
+            self.vokabel_fremd = daten[0][2]
+            self.buch = daten[0][3]
             
             #print "Richtung: ", self.Richtung
             
-            if self.Richtung == 1:
-                self.labVokabelMeintenSie.setText(self.vokabelDeutsch)
+            if self.richtung == 1:
+                self.labVokabelMeintenSie.setText(self.vokabel_deutsch)
             else:
-                self.labVokabelMeintenSie.setText(self.vokabelFremd)
+                self.labVokabelMeintenSie.setText(self.vokabel_fremd)
             
             self.labLektion.setText(str(self.lektion))
-            self.labBuch.setText(str(self.Buch))
+            self.labBuch.setText(str(self.buch))
             self.labRichtigFalsch.setText("")
             self.labBitteEingeben.setText("Bitte eingeben")
-            self.labWeitereVokabeln.setText("Noch "+str(len(self.vokabelIds)-self.idAktuell-1)+" weitere Vokabeln")
+            self.labWeitereVokabeln.setText("Noch "+str(len(self.vokabel_ids)-self.id_aktuell-1)+" weitere Vokabeln")
             self.tfInput.setText("")
             self.tfInput.setFocus()    
             self.labMeintenSie.setText("")
             
-            self.idAktuell +=1
+            self.id_aktuell +=1
         else:
             print "fertig mit Abfragen"
-    def LektionsIdToVokId(self, idliste, haeufigkeit):
-            
+
+    def lektionsid_to_vokid(self, idliste, haeufigkeit):
+        """
+        zu lektions id gibts die Vokabelid
+        """
         vokabelIds = []    
         for i in idliste:
-            daten = self.Datenbank.getDataAsList("select vokabeln.id from vokabeln \
+            daten = self.datenbank.getDataAsList("select vokabeln.id from vokabeln \
             join lektionen on (vokabeln.idlektion=lektionen.id) where lektionen.id like "+str(i))
             for n in daten:
                 for k in range(haeufigkeit):
@@ -109,29 +122,29 @@ class Abfrage(WindowAbfrage, QtGui.QWidget):
     def weitereVokabelKlick(self):
         
         self.thread.start()
-        if self.Richtung == 1:
+        if self.richtung == 1:
             #print type(self.vokabelFremd)
             #print type(str(self.tfInput.text().toUtf8()).decode("utf-8"))
-            if self.vokabelFremd == str(self.tfInput.text().toUtf8()).decode("utf-8"):
+            if self.vokabel_fremd == str(self.tfInput.text().toUtf8()).decode("utf-8"):
                 self.labRichtigFalsch.setText("Richtig")
                 self.labPunkte.setText(str(float(self.labPunkte.text()) + 1))
             else:
                 self.labRichtigFalsch.setText("Falsch")
-                if self.RichtigeAnzeigen:
-                    self.labBitteEingeben.setText(u"Richtig wäre: "+self.vokabelFremd)
-                if self.meintenSie:
-                    liste = self.Datenbank.getDataAsList("select fremd, id from vokabeln")# \
+                if self.richtige_anzeigen:
+                    self.labBitteEingeben.setText(u"Richtig wäre: "+self.vokabel_fremd)
+                if self.meinten_sie:
+                    liste = self.datenbank.getDataAsList("select fremd, id from vokabeln")# \
                     #where deutsch like '"+str(self.tfInput)+"'")
                     #print liste
-                    self.Treffer.setAktVergleich(liste, unicode(self.tfInput.text()), self.vokabelIds[self.idAktuell-1], 1)
+                    self.treffer.setAktVergleich(liste, unicode(self.tfInput.text()), self.vokabel_ids[self.id_aktuell-1], 1)
                     
                    
-                    daten = self.Datenbank.getDataAsList("select fremd, deutsch from vokabeln \
-                    "+ self.ListeZuSql(self.Treffer.getTreffer(), " id "))
+                    daten = self.datenbank.getDataAsList("select fremd, deutsch from vokabeln \
+                    "+ self.ListeZuSql(self.treffer.getTreffer(), " id "))
                     
                     
                     
-                    if self.Treffer.directStrike():
+                    if self.treffer.directStrike():
                         #print "direkterTreffer"
                         self.labRichtigFalsch.setText("fast richtig")
                         self.labPunkte.setText(str(float(self.labPunkte.text()) + 0.5))
@@ -161,31 +174,31 @@ class Abfrage(WindowAbfrage, QtGui.QWidget):
         else:
             #print "vergleich zwischen " + self.Vergeleichsfaehigkeit(self.vokabelDeutsch)
             #print self.Vergeleichsfaehigkeit(str(self.tfInput.text().toUtf8()).decode('utf-8'))
-            if self.Vergeleichsfaehigkeit(self.vokabelDeutsch) == self.Vergeleichsfaehigkeit(str(self.tfInput.text().toUtf8()).decode('utf-8')):
+            if self.Vergeleichsfaehigkeit(self.vokabel_deutsch) == self.Vergeleichsfaehigkeit(str(self.tfInput.text().toUtf8()).decode('utf-8')):
                 self.labRichtigFalsch.setText("Richtig")
                 self.labPunkte.setText(str(float(self.labPunkte.text()) + 1))
             else:
                 self.labRichtigFalsch.setText("Falsch")
-                if self.RichtigeAnzeigen:
-                    self.labBitteEingeben.setText(unicode(u"Richtig wäre: ")+str(self.vokabelDeutsch))
-                if self.meintenSie:
+                if self.richtige_anzeigen:
+                    self.labBitteEingeben.setText(unicode(u"Richtig wäre: ")+str(self.vokabel_deutsch))
+                if self.meinten_sie:
                     #print "meintenSie Aktiv"
-                    liste = self.Datenbank.getDataAsList("select deutsch, id from vokabeln")# \
+                    liste = self.datenbank.getDataAsList("select deutsch, id from vokabeln")# \
                     #where deutsch like '"+str(self.tfInput)+"'")
                     #print liste
-                    self.Treffer.setAktVergleich(liste, unicode(self.tfInput.text()), self.vokabelIds[self.idAktuell-1], 2)
+                    self.treffer.setAktVergleich(liste, unicode(self.tfInput.text()), self.vokabel_ids[self.id_aktuell-1], 2)
                     
                 
                     
                 
-                    daten = self.Datenbank.getDataAsList("select fremd, deutsch from vokabeln \
-                    "+ self.ListeZuSql(self.Treffer.getTreffer(), " id "))
+                    daten = self.datenbank.getDataAsList("select fremd, deutsch from vokabeln \
+                    "+ self.ListeZuSql(self.treffer.getTreffer(), " id "))
                     
                     """
                     if(len(daten) > 0):
                         self.labMeintenSie.setText("Meinten Sie: "+unicode(daten[0][0])+" - "+unicode(daten[0][1]))
                     """
-                    if self.Treffer.directStrike():
+                    if self.treffer.directStrike():
                         #print "direkter treffer"
                         self.labRichtigFalsch.setText("fast richtig")
                         self.labPunkte.setText(str(float(self.labPunkte.text()) + 0.5))
