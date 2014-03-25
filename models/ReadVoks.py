4f6026febb765744aa6108b20c7c1d1842049608
JPaehr <j.paehr@gmx.de>
2014-03-25 11:33:23 +0100

    fixed firstletterbug when loading vocabulary by file

--- models/ReadVoks.py
+++ models/ReadVoks.py
@@ -3,6 +3,7 @@ __author__ = 'JPaehr'
 
 import models.base as Datenbank
 from PyQt4 import QtGui
+import codecs
 
 
 class ReadVoks(object):
@@ -20,13 +21,16 @@ class ReadVoks(object):
         for lines in text:
             voks = lines.split("\t")
             if not voks[0] == "\n":
+                #print str(voks[0]).decode('utf-8').replace(u'\ufeff', "")
+                fremd = str(voks[0]).decode('utf-8').replace(u'\ufeff', "")
+
 
                 statement = "insert into vokabeln ('deutsch', fremd, idlektion) values ('"+str((voks[len(voks)-1]).strip('\n')).decode('utf-8')+"', " \
-                            "'"+str(voks[0]).decode('utf-8')+"', "+str(self.idLektion)+")"
+                            "'"+fremd+"', "+str(self.idLektion)+")"
+
                 #print statement
                 self.datenbank.setData(statement)
 
-                #print voks[0].strip("\n"), voks[len(voks)-1].strip("\n")
-
+#test = ReadVoks("D:\Downloads\\tilo\jay.txt", 1)
 
 #deutsch = str(self.tfDeutsch.text().toUtf8()).decode("utf-8").strip()
