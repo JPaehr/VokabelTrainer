ec88a69efbfd8426bbc8249393fc8a8b4d13cb03
JPaehr <j.paehr@gmx.de>
2021-12-04 21:16:28 +0100

    fix performance issue in statistics view

--- oberflaechen/Statistik.py
+++ oberflaechen/Statistik.py
@@ -22,7 +22,17 @@ class Statistik(WindowStatistik, QtGui.QWidget):
         self.datenbank = Datenbank.base("VokabelDatenbank.sqlite")
         statement = "select datum, richtig, gesamt, lektionen from statistik order by id desc"
         daten = self.datenbank.getDataAsList(statement)
+        print(daten)
         #print daten
+
+        ### create lookup table for lektion_id -> lektionen.name
+        statement = "select lektionen.name, lektionen.id from lektionen"
+        names = self.datenbank.getDataAsList(statement)
+        lektionen_dict = dict()
+        for i in names:
+            # lektionen_id : lektionen.name
+            lektionen_dict[i[1]] = i[0]
+        print(lektionen_dict)
         header = ['datum', 'richtig/von', 'in Prozent', 'lektionen']
 
         datenZumAnzeigen = []
@@ -33,10 +43,20 @@ class Statistik(WindowStatistik, QtGui.QWidget):
 
             lektionen = ""
             listeLektionId = i[3].split(",")
-            statement = "select lektionen.name from lektionen "+self.listeToSql(listeLektionId)
-            lektionsAbfrage = self.datenbank.getDataAsList(statement)
-            for teil in lektionsAbfrage:
-                lektionen = lektionen+unicode(teil[0])+", "
+
+            ### performance killer
+            #statement = "select lektionen.name from lektionen "+self.listeToSql(listeLektionId)
+            # print(statement)
+            #lektionsAbfrage = self.datenbank.getDataAsList(statement)
+            #print(lektionsAbfrage)
+
+            #for teil in lektionsAbfrage:
+            for teil in listeLektionId:
+                # print(teil)
+                try:
+                    lektionen = lektionen+unicode(lektionen_dict[int(teil)])+", "
+                except:
+                    print("unknown unit requested")
             lektionen = lektionen[:-2]
 
             datenZumAnzeigen.append([datum, richtigVon, inProzent, lektionen])
